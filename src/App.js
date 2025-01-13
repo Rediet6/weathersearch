d5abb865dba6e0ef36ab6d07c8892fc8da9713c2
Rediet6 <103461342+Rediet6@users.noreply.github.com>
2025-01-12 22:09:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <p>
-      <a href="https://github.com/Rediet6/Weather-react">Open-source code</a> by{" "}
+      <a href="https://github.com/Rediet6/weathersearch">Open-source code</a> by{" "}
       <a href="https://github.com/Rediet6">Rediet </a>
     </p>
   );
